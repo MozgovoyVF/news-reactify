5a1da27802f2ad34c6f074832545a710abcb27fb
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2024-07-17 14:51:03 +0300

    fix slider and banners scroll

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import styles from "./styles.module.css";
 
 interface ICategories {
@@ -6,32 +7,32 @@ interface ICategories {
   selectedCategory: string | null;
 }
 
-const Categories = ({
-  categories,
-  setSelectedCategory,
-  selectedCategory,
-}: ICategories) => {
-  return (
-    <div className={styles.categories}>
-      <button
-        onClick={() => setSelectedCategory(null)}
-        className={!selectedCategory ? styles.active : styles.item}
-      >
-        All
-      </button>
-      {categories.map((category) => (
+const Categories = forwardRef<HTMLDivElement, ICategories>(
+  ({ categories, setSelectedCategory, selectedCategory }: ICategories, ref) => {
+    return (
+      <div ref={ref} className={styles.categories}>
         <button
-          onClick={() => setSelectedCategory(category)}
-          className={
-            selectedCategory === category ? styles.active : styles.item
-          }
-          key={category}
+          onClick={() => setSelectedCategory(null)}
+          className={!selectedCategory ? styles.active : styles.item}
         >
-          {category}
+          All
         </button>
-      ))}
-    </div>
-  );
-};
+        {categories.map((category) => (
+          <button
+            onClick={() => setSelectedCategory(category)}
+            className={
+              selectedCategory === category ? styles.active : styles.item
+            }
+            key={category}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+    );
+  }
+);
+
+Categories.displayName = "Categories";
 
 export default Categories;
