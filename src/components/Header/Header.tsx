0358feba4a5352bb8f9ec2bc75d92bd0b6dd336e
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2024-07-22 13:40:06 +0300

    add context and dark theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
+import { themeIcons } from "../../assets";
+import { useTheme } from "../../context/ThemeContext";
 import { formatDate } from "../../helpers/formatDate";
 import styles from "./styles.module.css";
 
 const Header = () => {
+  const { isDark, toggleTheme } = useTheme();
+
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>NEW REACTIFY</h1>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+    <header
+      className={`${styles.header} ${isDark ? styles.dark : styles.light}`}
+    >
+      <div className={styles.info}>
+        <h1 className={styles.title}>NEW REACTIFY</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+      </div>
+
+      <img
+        src={isDark ? themeIcons.dark : themeIcons.light}
+        className={styles.image}
+        alt="icon"
+        onClick={toggleTheme}
+      />
     </header>
   );
 };
